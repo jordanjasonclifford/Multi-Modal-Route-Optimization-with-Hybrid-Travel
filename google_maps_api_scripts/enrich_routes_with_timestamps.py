b2c5d1ed4a466707379712c00e03272ab76ae050
Jordan Clifford <jordanjasonclifford@gmail.com>
2025-07-29 08:34:16 -0700

    09 week update, almost finished!

--- google_maps_api_scripts/enrich_routes_with_timestamps.py
+++ google_maps_api_scripts/enrich_routes_with_timestamps.py
@@ -111,5 +111,5 @@ for ts in timestamps:
 
 # Save to CSV
 out_df = pd.DataFrame(results)
-out_df.to_csv("enriched_routes_by_timestamp_wp_to_os.csv", index=False)
-print("\nAll done! Saved to enriched_routes_by_timestamp_wp_to_os.csv")
+out_df.to_csv("enriched_routes_by_timestamp_ppm_to_bh.csv", index=False)
+print("\nAll done! Saved to enriched_routes_by_timestamp_ppm_to_bh.csv")
